9f20d841a8e94105d89dce7df991c05acce558ba
Twometer <twometer@outlook.de>
2021-12-24 16:02:36 +0100

    Refactor and optimize world generator

--- src/world/gen.rs
+++ src/world/gen.rs
@@ -11,7 +11,7 @@ use crate::{
     world::World,
 };
 
-use super::{math::dist, ChunkPos};
+use super::{math::diff_opt, ChunkPos};
 
 pub struct WorldGenerator {
     config: WorldGenConfig,
@@ -50,8 +50,8 @@ impl WorldGenerator {
         let base_x = chunk.x << 4;
         let base_z = chunk.z << 4;
 
-        for x in 0..16 {
-            for z in 0..16 {
+        for z in 0..16 {
+            for x in 0..16 {
                 let world_x = base_x + x;
                 let world_z = base_z + z;
 
@@ -82,6 +82,7 @@ impl WorldGenerator {
             }
         }
 
+        // Let grass grow on top level dirt
         if top_layer_state == block_state!(3, 0) {
             chunk.set_block(x, top_layer_height, z, block_state!(2, 0));
         }
@@ -98,7 +99,7 @@ impl WorldGenerator {
 
         // Generate features
         for (feature, prob) in &biome.features {
-            if self.should_generate(*prob) {
+            if self.should_generate_feature(*prob) {
                 self.generate_feature(feature, chunk, x, top_layer_height, z);
             }
         }
@@ -192,33 +193,33 @@ impl WorldGenerator {
         trunk_block: u16,
         leaves_block: u16,
     ) {
-        if !Self::check_surroundings(chunk, x, y, z, 2, trunk_block) {
-            for i in 0..height {
-                if i > height - 5 {
-                    let r = (height - i).min(2);
+        if Self::has_nearby_block(chunk, x, y, z, 2, trunk_block) {
+            return;
+        }
+
+        for i in 0..height {
+            if i > height - 5 {
+                let r = (height - i).min(2);
+                for zo in -r..=r {
                     for xo in -r..=r {
-                        for zo in -r..=r {
-                            if i < height - 2
-                                || xo * xo + zo * zo <= r * r + rand::thread_rng().gen_range(0..1)
-                            {
-                                chunk.set_block(x + xo, y + i, z + zo, leaves_block)
-                            }
+                        if i < height - 2
+                            || xo * xo + zo * zo <= r * r + rand::thread_rng().gen_range(0..1)
+                        {
+                            chunk.set_block(x + xo, y + i, z + zo, leaves_block)
                         }
                     }
                 }
-
-                if i < height - 2 {
-                    chunk.set_block(x, y + i, z, trunk_block);
-                }
+            } else if i < height - 2 {
+                chunk.set_block(x, y + i, z, trunk_block);
             }
         }
     }
 
-    fn check_surroundings(chunk: &Chunk, x: i32, y: i32, z: i32, r: i32, state: u16) -> bool {
-        for xo in -r..=r {
-            for yo in -r..=r {
-                for zo in -r..=r {
-                    if chunk.get_block(x + xo, y + yo, z + zo) == state {
+    fn has_nearby_block(chunk: &Chunk, x: i32, y: i32, z: i32, r: i32, block: u16) -> bool {
+        for yo in -r..=r {
+            for zo in -r..=r {
+                for xo in -r..=r {
+                    if chunk.get_block(x + xo, y + yo, z + zo) == block {
                         return true;
                     }
                 }
@@ -227,43 +228,19 @@ impl WorldGenerator {
         return false;
     }
 
-    fn should_generate(&self, prob: f64) -> bool {
+    fn should_generate_feature(&self, prob: f64) -> bool {
         rand::thread_rng().gen_bool(prob)
     }
 
-    fn determine_block_underground(&self, x: i32, y: i32, z: i32) -> u16 {
-        for (_, ore) in &self.config.ores {
-            let offset = ore.id as f64 * 1000.0;
-            let noise = self.noise.get([
-                x as f64 * ore.scale,
-                y as f64 * ore.scale + offset,
-                z as f64 * ore.scale,
-            ]);
-
-            let diff = (ore.center - (y as f64)).abs();
-            if diff > ore.spread {
-                continue;
-            }
-
-            let offset = diff / ore.spread;
-            let threshold = ore.threshold + (offset * 0.055);
-            if noise > threshold {
-                return block_state!(ore.id, 0);
-            }
-        }
-
-        block_state!(1, 0)
-    }
-
     fn is_cave(&self, world_x: i32, y: i32, world_z: i32, h: i32) -> bool {
-        let n1 = self.sample_noise_fractal_3d(
+        let n1 = self.sample_cave_noise_fractal(
             world_x,
             y,
             world_z,
             self.config.cave_scale,
             self.config.cave_lac,
         );
-        let n2 = self.sample_noise_fractal_3d(
+        let n2 = self.sample_cave_noise_fractal(
             world_x + 1024,
             y,
             world_z + 1024,
@@ -285,51 +262,50 @@ impl WorldGenerator {
         gh: i32,
         biome: &BiomeConfig,
     ) -> u16 {
-        let is_cave = y <= th && self.is_cave(x, y, z, th);
-        let cave_block = if is_cave && y <= 8 {
-            11
-        } else if is_cave {
-            0
-        } else {
-            1
-        };
+        let is_bedrock = (y <= 3 && self.should_generate_feature(0.3)) || y == 0;
+        let can_cave = (!biome.sea_level || y < th - 3) && !is_bedrock;
+        let is_cave = y <= th && self.is_cave(x, y, z, th) && can_cave;
 
-        if y == gh {
-            if is_cave && !biome.sea_level {
-                block_state!(cave_block, 0)
-            } else {
-                block_state!(biome.blocks[0], 0)
-            }
+        if is_cave {
+            let cave_fill_block = if y <= 8 { 11 } else { 0 };
+            return block_state!(cave_fill_block, 0);
+        } else if y == gh {
+            return block_state!(biome.blocks[0], 0);
         } else if y >= th {
-            if is_cave && !biome.sea_level {
-                block_state!(cave_block, 0)
-            } else {
-                block_state!(biome.blocks[1], 0)
-            }
+            return block_state!(biome.blocks[1], 0);
         } else if y >= th - 3 {
-            if is_cave && !biome.sea_level {
-                block_state!(cave_block, 0)
-            } else {
-                block_state!(biome.blocks[2], 0)
-            }
+            return block_state!(biome.blocks[2], 0);
         } else if y > 3 {
-            if is_cave {
-                block_state!(cave_block, 0)
-            } else {
-                self.determine_block_underground(x, y, z)
-            }
-        } else if y > 0 {
-            block_state!(
-                if rand::thread_rng().gen_bool(0.5) {
-                    7
-                } else {
-                    cave_block
-                },
-                0
-            )
+            return self.determine_ore(x, y, z);
+        } else if is_bedrock {
+            return block_state!(7, 0);
         } else {
-            block_state!(7, 0)
+            return block_state!(1, 0);
         }
+    }
+
+    fn determine_ore(&self, x: i32, y: i32, z: i32) -> u16 {
+        for (_, ore) in &self.config.ores {
+            let diff = (ore.center - (y as f64)).abs();
+            if diff > ore.spread {
+                continue;
+            }
+
+            let noise_offset = ore.id as f64 * 1000.0;
+            let noise = self.noise.get([
+                x as f64 * ore.scale,
+                y as f64 * ore.scale + noise_offset,
+                z as f64 * ore.scale,
+            ]);
+
+            let offset = diff / ore.spread;
+            let threshold = ore.threshold + (offset * 0.055);
+            if noise > threshold {
+                return block_state!(ore.id, 0);
+            }
+        }
+
+        block_state!(1, 0)
     }
 
     fn multi_sample_biome_scale(&self, x: i32, z: i32, r: i32) -> f64 {
@@ -363,11 +339,11 @@ impl WorldGenerator {
 
         (
             elevation,
-            self.find_biome(temperature, moisture, elevation, river),
+            self.determine_biome(temperature, moisture, elevation, river),
         )
     }
 
-    fn find_biome(
+    fn determine_biome(
         &self,
         temperature: f64,
         moisture: f64,
@@ -392,9 +368,9 @@ impl WorldGenerator {
                 continue;
             }
 
-            let de = dist(elevation, biome.elevation);
-            let dt = dist(temperature, biome.temperature);
-            let dm = dist(moisture, biome.moisture);
+            let de = diff_opt(elevation, biome.elevation);
+            let dt = diff_opt(temperature, biome.temperature);
+            let dm = diff_opt(moisture, biome.moisture);
             let d = de * de + dt * dt + dm * dm;
             if d < best_biome_dist {
                 best_biome_dist = d;
@@ -422,7 +398,7 @@ impl WorldGenerator {
         result / denom
     }
 
-    fn sample_noise_fractal_3d(&self, x: i32, y: i32, z: i32, mut scale: f64, lac: f64) -> f64 {
+    fn sample_cave_noise_fractal(&self, x: i32, y: i32, z: i32, mut scale: f64, lac: f64) -> f64 {
         let mut result = 0.0;
         let mut denom = 0.0;
 
--- src/world/math.rs
+++ src/world/math.rs
@@ -1,7 +1,6 @@
-pub fn dist(a: f64, b: Option<f64>) -> f64 {
-    if b.is_none() {
-        0.0
-    } else {
-        b.unwrap() - a
+pub fn diff_opt(a: f64, b: Option<f64>) -> f64 {
+    match b {
+        Some(b) => b - a,
+        None => 0.0,
     }
 }
